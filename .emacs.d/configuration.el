7f639cbe50f0983885e48153068bb0d36f7c9174
Rahuketu86 <rahuketu86@gmail.com>
2016-06-27 23:13:07 +0400

    Added Org configuration and package repositories.

--- .emacs.d/configuration.el
+++ .emacs.d/configuration.el
@@ -1,4 +1,20 @@
-
 (setq custom-file
       (expand-file-name "custom.el"
                         user-emacs-directory))
+
+'(require 'package)
+;;;(add-to-list 'package-archives
+;;;'             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(defun my-config()
+  "Function to open org configuration file"
+  (interactive)
+;;  (find-file load-file-name))
+  (find-file (expand-file-name "configuration.org"
+                               user-emacs-directory)))
